11283e7b7fb2b1cc5f22bc97eccb35084cbfdfdf
Sander van Leeuwen <djsavale@hotmail.com>
2014-06-17 23:32:16 +0200

    fixed nnonce

--- src/main.cpp
+++ src/main.cpp
@@ -2591,7 +2591,7 @@ bool LoadBlockIndex(bool fAllowNew)
         block.nNonce   = 5726282;
         if(fTestNet)
         {
-            block.nNonce   = 1908795;
+            block.nNonce   = 1912346;
         }
         if (true  && (block.GetHash() != hashGenesisBlock)) {
 
